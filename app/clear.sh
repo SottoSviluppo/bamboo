2b3d85c7f9031e22b35c00e7add4d402a2ffc94d
Claudio Conforti <ilconfo@gmail.com>
2016-10-20 15:52:28 +0200

    Clear updated

--- app/clear.sh
+++ app/clear.sh
@@ -5,6 +5,8 @@ echo -e \\n"> rm -rf /dev/shm/project/cache/dev/"
 rm -rf /dev/shm/project/cache/dev/
 echo -e \\n"> rm -rf /dev/shm/project/cache/de_/"
 rm -rf /dev/shm/project/cache/de_/
+echo -e \\n"> rm -rf app/cache/prod"
+rm -rf app/cache/prod/
 echo -e \\n"> /usr/bin/env php app/console cache:clear"
 /usr/bin/env php app/console cache:clear
 echo -e \\n"> redis-cli flushall"
