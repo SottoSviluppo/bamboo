d94383d1966a868f7894f832540a12505567f2f8
albi.mori <albi.mori@gmail.com>
2017-01-02 13:01:58 +0100

    add permissions check to menu items

--- src/Elcodi/Admin/UserBundle/Builder/MenuBuilder.php
+++ src/Elcodi/Admin/UserBundle/Builder/MenuBuilder.php
@@ -20,12 +20,38 @@ namespace Elcodi\Admin\UserBundle\Builder;
 use Elcodi\Component\Menu\Builder\Abstracts\AbstractMenuBuilder;
 use Elcodi\Component\Menu\Builder\Interfaces\MenuBuilderInterface;
 use Elcodi\Component\Menu\Entity\Menu\Interfaces\MenuInterface;
+use Elcodi\Component\Menu\Factory\NodeFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class MenuBuilder
  */
 class MenuBuilder extends AbstractMenuBuilder implements MenuBuilderInterface
 {
+    private $permissionsRepository;
+    private $currentUser;
+    private $resource = "customer";
+    private $permissions = [
+        'canRead' => false,
+        'canCreate' => false,
+        'canUpdate' => false,
+        'canDelete' => false
+    ];
+
+    function __construct(NodeFactory $menuNodeFactory, ContainerInterface $container)
+    {
+        parent::__construct($menuNodeFactory);
+        $this->permissionsRepository = $container->get('elcodi.repository.permission_group');
+        $this->currentUser = $container->get('security.token_storage')->getToken()->getUser();
+
+        $this->permissions = [
+            'canRead' => $this->permissionsRepository->canReadEntity($this->resource, $this->currentUser),
+            'canCreate' => $this->permissionsRepository->canCreateEntity($this->resource, $this->currentUser),
+            'canUpdate' => $this->permissionsRepository->canUpdateEntity($this->resource, $this->currentUser),
+            'canDelete' => $this->permissionsRepository->canDeleteEntity($this->resource, $this->currentUser)
+        ];
+    }
+
     /**
      * Build the menu
      *
@@ -33,19 +59,23 @@ class MenuBuilder extends AbstractMenuBuilder implements MenuBuilderInterface
      */
     public function build(MenuInterface $menu)
     {
-        $menu
-            ->addSubnode(
-                $this
-                    ->menuNodeFactory
-                    ->create()
-                    ->setName('admin.customer.plural')
-                    ->setCode('user')
-                    ->setUrl('admin_customer_list')
-                    ->setTag('order')
-                    ->setPriority(32)
-                    ->setActiveUrls([
-                        'admin_customer_edit',
-                    ])
-            );
+        if ($this->permissions['canRead']) {
+            $customerNode = $this
+                                ->menuNodeFactory
+                                ->create()
+                                ->setName('admin.customer.plural')
+                                ->setCode('user')
+                                ->setUrl('admin_customer_list')
+                                ->setTag('order')
+                                ->setPriority(32);
+
+            if ($this->permissions['canUpdate']) {
+                $customerNode->setActiveUrls([
+                    'admin_customer_edit',
+                ]);
+            }
+
+            $menu->addSubnode($customerNode);
+        }
     }
 }
