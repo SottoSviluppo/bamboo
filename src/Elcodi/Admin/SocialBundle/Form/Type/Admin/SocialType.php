7c47930cdafc6c6d8df99d0c6a4d5280376a7ffb
Claudio Conforti <ilconfo@gmail.com>
2017-04-26 11:20:10 +0000

    Social bundle with link translated

--- src/Elcodi/Admin/SocialBundle/Form/Type/Admin/SocialType.php
+++ src/Elcodi/Admin/SocialBundle/Form/Type/Admin/SocialType.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
- * This file is part of the Elcodi package.
- *
- * Copyright (c) 2014-2016 Elcodi.com
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * Feel free to edit as you please, and have fun.
- *
- * @author Marc Morera <yuhu@mmoreram.com>
- * @author Aldo Chiecchia <zimage@tiscali.it>
- * @author Elcodi Team <tech@elcodi.com>
- */
 
 namespace Elcodi\Admin\SocialBundle\Form\Type\Admin;
 
@@ -21,24 +7,15 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 use Elcodi\Component\Core\Factory\Traits\FactoryTrait;
 use Elcodi\Component\EntityTranslator\EventListener\Traits\EntityTranslatableFormTrait;
 
-
-/**
- * Class SocialType
- */
 class SocialType extends AbstractType
 {
     use FactoryTrait
+    ,EntityTranslatableFormTrait
     ;
 
-
-    /**
-     * Construct
-     *
-     */
     public function __construct(
     ) {
     }
@@ -87,6 +64,7 @@ class SocialType extends AbstractType
                 'required' => false,
             ]);
 
+        $builder->addEventSubscriber($this->getEntityTranslatorFormEventListener());
     }
 
     /**
