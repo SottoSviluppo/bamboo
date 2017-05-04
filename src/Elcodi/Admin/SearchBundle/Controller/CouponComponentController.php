83d0265bc5189ebad5275bb42f4751a25a85603e
Claudio Conforti <ilconfo@gmail.com>
2017-05-04 12:15:48 +0200

    Search bettered in orders admin, with some refactoring

--- src/Elcodi/Admin/SearchBundle/Controller/CouponComponentController.php
+++ src/Elcodi/Admin/SearchBundle/Controller/CouponComponentController.php
@@ -3,6 +3,8 @@
 namespace Elcodi\Admin\SearchBundle\Controller;
 
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Elcodi\Admin\CoreBundle\Controller\Abstracts\AbstractAdminController;
+use Elcodi\Component\Product\Entity\Interfaces\ProductInterface;
 use Mmoreram\ControllerExtraBundle\Annotation\Entity as EntityAnnotation;
 use Mmoreram\ControllerExtraBundle\Annotation\Form as FormAnnotation;
 use Mmoreram\ControllerExtraBundle\Annotation\Paginator as PaginatorAnnotation;
@@ -11,34 +13,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Form\FormView;
 
-use Elcodi\Admin\CoreBundle\Controller\Abstracts\AbstractAdminController;
-use Elcodi\Component\Product\Entity\Interfaces\ProductInterface;
-
 class CouponComponentController extends AbstractAdminController
 {
     private $service;
 
-    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container=null)
+    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null)
     {
         parent::setContainer($container);
         $this->service = $this->get('elcodi_admin.admin_search');
     }
 
     /**
-    * @Template("AdminCouponBundle:Coupon:listComponent.html.twig")
-    */
+     * @Template("AdminCouponBundle:Coupon:listComponent.html.twig")
+     */
     public function listComponentAction($query, $page, $limit = null)
     {
         $coupons = $this->service->searchCoupons($query, $page, $limit);
         return [
-                'query' => $query,
-                'paginator' => $coupons,
-                'page' => $page,
-                'limit' => $this->service->getLimit(),
-                'orderByField' => '',
-                'orderByDirection' => '',
-                'totalPages' => ceil($coupons->getTotalItemCount()/$this->service->getLimit()),
-                'totalElements'=> $coupons->getTotalItemCount(),
-            ];
+            'query' => $query,
+            'paginator' => $coupons,
+            'page' => $page,
+            'limit' => $this->service->getLimit(),
+            'orderByField' => 'id',
+            'orderByDirection' => 'DESC',
+            'totalPages' => ceil($coupons->getTotalItemCount() / $this->service->getLimit()),
+            'totalElements' => $coupons->getTotalItemCount(),
+        ];
     }
 }
