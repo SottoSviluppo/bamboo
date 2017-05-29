16e02718ab3414c8b57095bb97b753f318601c6f
Claudio Conforti <ilconfo@gmail.com>
2017-05-29 17:32:22 +0200

    Color on orders coupon

--- src/Elcodi/Admin/SearchBundle/Controller/OrderComponentController.php
+++ src/Elcodi/Admin/SearchBundle/Controller/OrderComponentController.php
@@ -41,6 +41,9 @@ class OrderComponentController extends AbstractAdminController
 
         $countries = $this->get('elcodi.repository.country')->findByEnabled(true);
 
+        $orders = $ordersPaginator->getItems();
+        $orderCoupons = $this->getCouponsFromOrders($orders);
+
         $results = [
             'paginator' => $ordersPaginator,
             'orderByField' => 'id',
@@ -48,6 +51,7 @@ class OrderComponentController extends AbstractAdminController
             'totalPages' => ceil($ordersPaginator->getTotalItemCount() / $limit),
             'totalElements' => $ordersPaginator->getTotalItemCount(),
             'countries' => $countries,
+            'orderCoupons' => $orderCoupons,
         ];
         $results = array_merge($results, $searchParameters);
 
@@ -55,6 +59,23 @@ class OrderComponentController extends AbstractAdminController
             $searchParameters['template'],
             $results
         );
+    }
+
+    public function getCouponsFromOrders($orders)
+    {
+        $coupons = array();
+        foreach ($orders as $order) {
+            //Cerca i coupon associati all'ordine passato
+            $orderCoupons = $this->get('elcodi.repository.order_coupon')->findOrderCouponsByOrder($order);
+            if (empty($orderCoupons)) {
+                $coupon = null;
+            } else {
+                $coupon = $orderCoupons[0]->getCoupon();
+            }
+            //Array dei coupons associati agli ordini, array chiave valore, dove la chiave è l'id dell'ordine mentre il valore è il coupon se presente, altrimenti ha valore NULL
+            $coupons[$order->getId()] = $coupon;
+        }
+        return $coupons;
     }
 
 }
