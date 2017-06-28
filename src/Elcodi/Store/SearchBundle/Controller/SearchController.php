acf488a5d5d55f6988ea11db1649d8492f708b41
Claudio Conforti <ilconfo@gmail.com>
2017-06-28 22:04:44 +0200

    upgrades to search in store

--- src/Elcodi/Store/SearchBundle/Controller/SearchController.php
+++ src/Elcodi/Store/SearchBundle/Controller/SearchController.php
@@ -2,17 +2,16 @@
 
 namespace Elcodi\Store\SearchBundle\Controller;
 
-use Mmoreram\ControllerExtraBundle\Annotation\Entity as AnnotationEntity;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
 use Elcodi\Component\Product\Entity\Interfaces\CategoryInterface;
 use Elcodi\Component\Product\Entity\Interfaces\PurchasableInterface;
 use Elcodi\Component\Product\Repository\CategoryRepository;
 use Elcodi\Component\Product\Repository\PurchasableRepository;
 use Elcodi\Store\CoreBundle\Controller\Traits\TemplateRenderTrait;
+use Mmoreram\ControllerExtraBundle\Annotation\Entity as AnnotationEntity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Defines all the search methods on frontend
@@ -23,7 +22,7 @@ class SearchController extends Controller
 
     private $service;
 
-    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container=null)
+    public function setContainer(\Symfony\Component\DependencyInjection\ContainerInterface $container = null)
     {
         parent::setContainer($container);
         $this->service = $this->get('elcodi_store.store_search');
@@ -50,10 +49,20 @@ class SearchController extends Controller
         if (empty($page)) {
             $page = 1;
         }
-        
+
         $limit = $request->query->get('limit');
         if (empty($limit)) {
             $limit = null;
+        }
+
+        $sortField = $request->query->get('sortField', '');
+        $sortDirection = $request->query->get('sortDirection', 'ASC');
+
+        if ($sortField != '') {
+            $this->service->setSortArray(array(
+                $sortField => $sortDirection,
+            )
+            );
         }
 
         $categoryConnector = $request->query->get('catcn');
@@ -61,7 +70,7 @@ class SearchController extends Controller
             $categoryConnector = null;
         }
 
-        $products = $this->service->searchProducts($query, $page, $limit, $categories, $priceRange, $categoryConnector);
+        $products = $this->service->searchProducts($query, $page, $limit, $categories, $priceRange, $categoryConnector, $sortField);
 
         return $this->renderTemplate(
             'Pages:search-products.html.twig',
