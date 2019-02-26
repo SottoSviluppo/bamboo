39454675dae60a371762d4f322eaac5b214438ff
Emanuela Davelli <emanuela.davelli@sottosviluppo.com>
2019-02-26 15:52:36 +0100

    partial search for products

--- src/Elcodi/Store/SearchBundle/Services/StoreSearchService.php
+++ src/Elcodi/Store/SearchBundle/Services/StoreSearchService.php
@@ -4,11 +4,8 @@ namespace Elcodi\Store\SearchBundle\Services;
 
 use Elastica\Query;
 use Elastica\Query\BoolQuery;
-use Elastica\Query\Filtered;
-use Elastica\Query\Match;
 use Elastica\Query\MultiMatch;
 use Elastica\Query\Nested;
-use Elastica\Query\QueryString;
 use Elastica\Query\Range;
 use Elastica\Query\Term;
 use Elastica\Query\Terms;
@@ -20,288 +17,238 @@ use Elcodi\Store\SearchBundle\Services\IStoreSearchService;
 /**
  * Defines all the search methods
  */
-class StoreSearchService implements IStoreSearchService
-{
-    protected $container;
-    protected $prefix;
-    protected $paginator;
-    protected $itemsPerPage;
-    protected $currencyConverter;
-    protected $currencyRepository;
-    protected $currentCurrency;
-    protected $defaultCurrency;
-    protected $categoryDefaultConnector;
-    protected $productPartialSearch;
-    protected $categoryPartialSearch;
-    protected $searchProductsConnector;
-    protected $searchProductsWithVariants;
+class StoreSearchService implements IStoreSearchService {
+	protected $container;
+	protected $prefix;
+	protected $paginator;
+	protected $itemsPerPage;
+	protected $currencyConverter;
+	protected $currencyRepository;
+	protected $currentCurrency;
+	protected $defaultCurrency;
+	protected $categoryDefaultConnector;
+	protected $productPartialSearch;
+	protected $categoryPartialSearch;
+	protected $searchProductsConnector;
+	protected $searchProductsWithVariants;
 
-    protected $sortArray = array(
-        'principalCategory.name' => array('order' => 'asc'),
-        'id' => array('order' => 'asc'),
-    );
+	protected $sortArray = array(
+		'principalCategory.name' => array('order' => 'asc'),
+		'id' => array('order' => 'asc'),
+	);
 
-    public function setSortArray($sortArray)
-    {
-        $this->sortArray = $sortArray;
-    }
+	public function setSortArray($sortArray) {
+		$this->sortArray = $sortArray;
+	}
 
-    public function __construct(
-        \Symfony\Component\DependencyInjection\ContainerInterface $container,
-        $prefix,
-        $itemsPerPage,
-        CurrencyConverter $currencyConverter,
-        $categoryDefaultConnector,
-        $productPartialSearch,
-        $categoryPartialSearch,
-        $searchProductsConnector,
-        $searchProductsWithVariants
-    ) {
-        $this->container = $container;
-        $this->prefix = $prefix;
-        $this->itemsPerPage = $itemsPerPage;
-        $this->currencyConverter = $currencyConverter;
-        $this->categoryDefaultConnector = $categoryDefaultConnector;
-        $this->productPartialSearch = $productPartialSearch;
-        $this->categoryPartialSearch = $categoryPartialSearch;
+	public function __construct(
+		\Symfony\Component\DependencyInjection\ContainerInterface $container,
+		$prefix,
+		$itemsPerPage,
+		CurrencyConverter $currencyConverter,
+		$categoryDefaultConnector,
+		$productPartialSearch,
+		$categoryPartialSearch,
+		$searchProductsConnector,
+		$searchProductsWithVariants
+	) {
+		$this->container = $container;
+		$this->prefix = $prefix;
+		$this->itemsPerPage = $itemsPerPage;
+		$this->currencyConverter = $currencyConverter;
+		$this->categoryDefaultConnector = $categoryDefaultConnector;
+		$this->productPartialSearch = $productPartialSearch;
+		$this->categoryPartialSearch = $categoryPartialSearch;
 
-        $this->paginator = $this->container->get('knp_paginator');
-        $this->currencyRepository = $this->container->get('elcodi.repository.currency');
-        $this->currentCurrency = $this->container->get('elcodi.wrapper.currency')->get();
-        $this->defaultCurrency = $this->container->get('elcodi.wrapper.default_currency')->get();
-        $this->searchProductsConnector = $searchProductsConnector;
-        $this->searchProductsWithVariants = $searchProductsWithVariants;
-    }
+		$this->paginator = $this->container->get('knp_paginator');
+		$this->currencyRepository = $this->container->get('elcodi.repository.currency');
+		$this->currentCurrency = $this->container->get('elcodi.wrapper.currency')->get();
+		$this->defaultCurrency = $this->container->get('elcodi.wrapper.default_currency')->get();
+		$this->searchProductsConnector = $searchProductsConnector;
+		$this->searchProductsWithVariants = $searchProductsWithVariants;
+	}
 
-    public function searchProducts($query, $page = 1, $limit = null, array $categories = array(), array $priceRange = array(), $categoryConnector = null)
-    {
-        if ($query == '') {
-            $query = '_';
-        }
+	public function searchProducts($query, $page = 1, $limit = null, array $categories = array(), array $priceRange = array(), $categoryConnector = null) {
+		if ($query == '') {
+			$query = '_';
+		}
 
-        if (empty($limit)) {
-            $limit = $this->itemsPerPage;
-        }
+		if (empty($limit)) {
+			$limit = $this->itemsPerPage;
+		}
 
-        if (empty($categoryConnector)) {
-            $categoryConnector = $this->categoryDefaultConnector;
-        }
+		if (empty($categoryConnector)) {
+			$categoryConnector = $this->categoryDefaultConnector;
+		}
 
-        $finder = $this->createFinderFor('products');
+		$finder = $this->createFinderFor('products');
 
-        //$adapter = $finder->createPaginatorAdapter('*'.$query.'*');
-        $productQuery = $this->createQueryForProducts($query, $categories, $priceRange, $categoryConnector);
-        // $boolQuery->setSort(array('id' => array('order' => 'asc')));
+		$productQuery = $this->createQueryForProducts($query, $categories, $priceRange, $categoryConnector);
 
-        $adapter = $finder->createPaginatorAdapter($productQuery);
-        return $this->paginator->paginate($adapter, $page, $limit);
-    }
+		$adapter = $finder->createPaginatorAdapter($productQuery);
+		return $this->paginator->paginate($adapter, $page, $limit);
+	}
 
-    protected function createFinderFor($type)
-    {
-        return $this->container->get('fos_elastica.finder.app.' . $type);
-    }
+	protected function createFinderFor($type) {
+		return $this->container->get('fos_elastica.finder.app.' . $type);
+	}
 
-    protected function createQueryForProducts($query, array $categories = array(), array $priceRange = array(), $categoryConnector)
-    {
-        $boolQuery = $this->getBoolQueryForProducts($query, $categories, $priceRange, $categoryConnector);
+	protected function createQueryForProducts($query, array $categories = array(), array $priceRange = array(), $categoryConnector) {
+		$boolQuery = $this->getBoolQueryForProducts($query, $categories, $priceRange, $categoryConnector);
 
-        // ordina i risultati della ricerca per principalCategory e per id del prodotto
-        $finalQuery = new Query($boolQuery);
+		// ordina i risultati della ricerca per principalCategory e per id del prodotto
+		$finalQuery = new Query($boolQuery);
 
-        if (empty($query) and !empty($categories)) {
-            $finalQuery->setSort($this->sortArray);
-        }
+		if (empty($query) and !empty($categories)) {
+			$finalQuery->setSort($this->sortArray);
+		}
+		return $finalQuery;
+	}
 
-        return $finalQuery;
-    }
+	protected function getBoolQueryForProducts($query, array $categories = array(), array $priceRange = array(), $categoryConnector) {
+		$boolQuery = new BoolQuery();
 
-    protected function getBoolQueryForProducts($query, array $categories = array(), array $priceRange = array(), $categoryConnector)
-    {
-        $boolQuery = new BoolQuery();
+		$enableQuery = new Term();
+		$enableQuery->setTerm('enabled', true);
+		$enableQuery->setTerm('private', false);
+		$boolQuery->addFilter($enableQuery);
 
-        $enableQuery = new Term();
-        $enableQuery->setTerm('enabled', true);
-        $enableQuery->setTerm('private', false);
-        $boolQuery->addFilter($enableQuery);
+		if (!empty($query)) {
+			$query = trim($query);
+			$baseQuery = new BoolQuery();
 
-        if (!empty($query)) {
-            $query = trim($query);
-            $baseQuery = new BoolQuery();
+			if ($this->productPartialSearch) {
+				$productsQuery = $this->setQueryForPartialProducts($query);
+			} else {
+				$productsQuery = $this->setQueryForProducts($query);
+			}
+			$baseQuery->addShould($productsQuery);
 
-            $productsQuery = $this->setQueryForProducts($query);
-            $baseQuery->addShould($productsQuery);
+			if ($this->searchProductsWithVariants) {
+				$variantsQuery = $this->setNestedQueriesForVariants($query);
+				$baseQuery->addShould($variantsQuery);
+			}
+			$boolQuery->addMust($baseQuery);
+		}
 
-            if ($this->searchProductsWithVariants) {
-                $variantsQuery = $this->setNestedQueriesForVariants($query);
-                $baseQuery->addShould($variantsQuery);
-            }
-            $boolQuery->addMust($baseQuery);
-        }
+		if (!empty($categories)) {
+			$this->setCategoriesQuery($boolQuery, $categories, $categoryConnector);
+		}
 
-        //debugging
-        // echo "<pre>";
-        // print_r($boolQuery->toArray());die();
+		if (!empty($priceRange)) {
+			$this->setPriceRangeQuery($boolQuery, $priceRange);
+		}
+		return $boolQuery;
+	}
 
-        if (!empty($categories)) {
-            $this->setCategoriesQuery($boolQuery, $categories, $categoryConnector);
-        }
+	protected function setQueryForProducts($query) {
+		$totalProductsQuery = new BoolQuery();
+		$tokens = explode(' ', $query);
+		foreach ($tokens as $token) {
+			$token = trim($token);
 
-        if (!empty($priceRange)) {
-            $this->setPriceRangeQuery($boolQuery, $priceRange);
-        }
-        return $boolQuery;
-    }
+			$productsQuery = new MultiMatch();
+			$productsQuery->setQuery($token);
+			$productsQuery->setFields([
+				'name', 'shortDescription', 'description', 'sku',
+			]);
+			if ($this->searchProductsConnector == 'or') {
+				$totalProductsQuery->addShould($productsQuery);
+			} else {
+				$totalProductsQuery->addMust($productsQuery);
+			}
+		}
+		return $totalProductsQuery;
+	}
 
-    protected function setQueryForProducts($query)
-    {
-        $totalProductsQuery = new BoolQuery();
-        $tokens = explode(' ', $query);
-        foreach ($tokens as $token) {
-            $token = trim($token);
+	/**
+	 * Query per la ricerca parziale dei prodotti, cercado in name shortDescription, description, sku
+	 * @param string $query stringa da cercare
+	 */
+	protected function setQueryForPartialProducts($query) {
+		$query = strtolower($query);
+		$wildcardBool = new BoolQuery();
 
-            $productsQuery = new MultiMatch();
-            $productsQuery->setQuery($token);
-            $productsQuery->setFields([
-                'name', 'shortDescription', 'description', 'sku',
-            ]);
-            if ($this->searchProductsConnector == 'or') {
-                $totalProductsQuery->addShould($productsQuery);
-            } else {
-                $totalProductsQuery->addMust($productsQuery);
-            }
-        }
-        return $totalProductsQuery;
-    }
+		$wildcardBool->addShould(new Wildcard('name', '*' . $query . '*'));
+		$wildcardBool->addShould(new Wildcard('shortDescription', '*' . $query . '*'));
+		$wildcardBool->addShould(new Wildcard('description', '*' . $query . '*'));
+		$wildcardBool->addShould(new Wildcard('sku', '*' . $query . '*'));
 
-    protected function setNestedQueriesForVariants($query)
-    {
-        $totalVariantsQuery = new BoolQuery();
-        $tokens = explode(' ', $query);
-        foreach ($tokens as $token) {
-            $token = trim($token);
+		return $wildcardBool;
+	}
 
-            $variants = new Nested();
-            $variants->setPath('variants');
-            $variantsQuery = new MultiMatch();
-            $variantsQuery->setQuery($token);
-            $variantsQuery->setFields([
-                'variants.name', 'variants.shortDescription', 'variants.description', 'variants.sku',
-            ]);
+	protected function setNestedQueriesForVariants($query) {
+		$totalVariantsQuery = new BoolQuery();
+		$tokens = explode(' ', $query);
+		foreach ($tokens as $token) {
+			$token = trim($token);
 
-            if ($this->searchProductsConnector == 'or') {
-                $totalVariantsQuery->addShould($variantsQuery);
-            } else {
-                $totalVariantsQuery->addMust($variantsQuery);
-            }
-        }
-        return $totalVariantsQuery;
-    }
+			$variants = new Nested();
+			$variants->setPath('variants');
+			$variantsQuery = new MultiMatch();
+			$variantsQuery->setQuery($token);
+			$variantsQuery->setFields([
+				'variants.name', 'variants.shortDescription', 'variants.description', 'variants.sku',
+			]);
 
-    /*protected function setNestedQueriesForProduct(BoolQuery $boolQuery, $query)
-    {
-    $categories = new Nested();
-    $categories->setPath('categories');
+			if ($this->searchProductsConnector == 'or') {
+				$totalVariantsQuery->addShould($variantsQuery);
+			} else {
+				$totalVariantsQuery->addMust($variantsQuery);
+			}
+		}
+		return $totalVariantsQuery;
+	}
 
-    $categoriesQuery = new BoolQuery();
+	protected function setPriceRangeQuery(BoolQuery $boolQuery, array $priceRange) {
+		$priceRange = array_map(function ($item) {
+			$item = floatval($item);
+			$money = $this->currencyConverter->convertMoney(Money::create($item * $this->currentCurrency->getDivideBy(), $this->currentCurrency), $this->defaultCurrency);
 
-    $categoryNameQuery = new Wildcard('categories.name', '*' . $query . '*');
-    if (!$this->categoryPartialSearch) {
-    $categoryNameQuery = new Match('categories.name', $query);
-    }
-    $categoriesQuery->addShould($categoryNameQuery);
+			return $money->getAmount();
+		}, $priceRange);
 
-    $categories->setQuery($categoriesQuery);
+		$range = [
+			'from' => $priceRange[0],
+		];
 
-    $boolQuery->addShould($categories);
+		if (count($priceRange) > 1) {
+			$range['to'] = $priceRange[1];
+		}
 
-    $variants = new Nested();
-    $variants->setPath('variants');
-    $variantsQuery = new MultiMatch();
-    $variantsQuery->setQuery($query);
-    $variantsQuery->setFields([
-    'variants.shortDescription', 'variants.description',
-    ]);
+		$priceQuery = new Range('price.amount', $range);
+		$boolQuery->addMust($priceQuery);
+	}
 
-    $variantsBool = new BoolQuery();
-    $variantsBool->addShould($variantsQuery);
+	protected function setCategoriesQuery(BoolQuery $boolQuery, array $categories, $categoryConnector) {
+		$categoriesQuery = new Nested();
+		$categoriesQuery->setPath('categories');
 
-    $variantNameQuery = new Match('variants.name', $query);
-    if ($this->productPartialSearch) {
-    $variantNameQuery = new Wildcard('variants.name', '*' . $query . '*');
-    }
-    $variantsBool->addShould($variantNameQuery);
+		$categoriesBool = new BoolQuery();
+		if ($categoryConnector == 'or') {
+			$categoriesBool->addMust(new Terms('categories.id', $categories));
+		} elseif ($categoryConnector == 'and') {
+			foreach ($categories as $category) {
+				$categoryIdNested = new Nested();
+				$categoryIdNested->setPath('categories');
 
-    $variantsSkuQuery = new Match('variants.sku', $query);
-    if ($this->productPartialSearch) {
-    $variantsSkuQuery = new Wildcard('variants.sku', '*' . $query . '*');
-    }
-    $variantsBool->addShould($variantsSkuQuery);
+				$categoryIdBool = new BoolQuery();
 
-    $variants->setQuery($variantsBool);
+				$categoryId = new Term();
+				$categoryId->setTerm('categories.id', $category);
+				$categoryIdBool->addMust($categoryId);
 
-    $boolQuery->addShould($variants);
+				$categoryIdNested->setQuery($categoryIdBool);
+				$boolQuery->addMust($categoryIdNested);
+			}
+		}
 
-    $manufacturers = new BoolQuery();
-    $manufacturerNameQuery = new Wildcard('manufacturer.name', '*' . $query . '*');
-    if (!$this->categoryPartialSearch) {
-    $manufacturerNameQuery = new Match('manufacturer.name', $query);
-    }
-    $manufacturers->addShould($manufacturerNameQuery);
-    $boolQuery->addShould($manufacturers);
-    }*/
+		$enableQuery = new Term();
+		$enableQuery->setTerm('categories.enabled', true);
+		$categoriesBool->addFilter($enableQuery);
 
-    protected function setPriceRangeQuery(BoolQuery $boolQuery, array $priceRange)
-    {
-        $priceRange = array_map(function ($item) {
-            $item = floatval($item);
-            $money = $this->currencyConverter->convertMoney(Money::create($item * $this->currentCurrency->getDivideBy(), $this->currentCurrency), $this->defaultCurrency);
+		$categoriesQuery->setQuery($categoriesBool);
 
-            return $money->getAmount();
-        }, $priceRange);
-
-        $range = [
-            'from' => $priceRange[0],
-        ];
-
-        if (count($priceRange) > 1) {
-            $range['to'] = $priceRange[1];
-        }
-
-        $priceQuery = new Range('price.amount', $range);
-        $boolQuery->addMust($priceQuery);
-    }
-
-    protected function setCategoriesQuery(BoolQuery $boolQuery, array $categories, $categoryConnector)
-    {
-        $categoriesQuery = new Nested();
-        $categoriesQuery->setPath('categories');
-
-        $categoriesBool = new BoolQuery();
-        if ($categoryConnector == 'or') {
-            $categoriesBool->addMust(new Terms('categories.id', $categories));
-        } elseif ($categoryConnector == 'and') {
-            foreach ($categories as $category) {
-                $categoryIdNested = new Nested();
-                $categoryIdNested->setPath('categories');
-
-                $categoryIdBool = new BoolQuery();
-
-                $categoryId = new Term();
-                $categoryId->setTerm('categories.id', $category);
-                $categoryIdBool->addMust($categoryId);
-
-                $categoryIdNested->setQuery($categoryIdBool);
-                $boolQuery->addMust($categoryIdNested);
-            }
-        }
-
-        $enableQuery = new Term();
-        $enableQuery->setTerm('categories.enabled', true);
-        $categoriesBool->addFilter($enableQuery);
-
-        $categoriesQuery->setQuery($categoriesBool);
-
-        $boolQuery->addMust($categoriesQuery);
-    }
+		$boolQuery->addMust($categoriesQuery);
+	}
 }
