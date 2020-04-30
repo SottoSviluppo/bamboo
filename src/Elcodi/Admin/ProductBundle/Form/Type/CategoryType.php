4771c76155005aee0e0f4d6130b1e5b8a6b09f55
Emanuela Davelli <emanuela.davelli@sottosviluppo.com>
2020-04-30 12:03:54 +0200

    add category description

--- src/Elcodi/Admin/ProductBundle/Form/Type/CategoryType.php
+++ src/Elcodi/Admin/ProductBundle/Form/Type/CategoryType.php
@@ -20,6 +20,7 @@ namespace Elcodi\Admin\ProductBundle\Form\Type;
 use Elcodi\Component\Core\Factory\Traits\FactoryTrait;
 use Elcodi\Component\EntityTranslator\EventListener\Traits\EntityTranslatableFormTrait;
 use Elcodi\Component\Product\Repository\CategoryRepository;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,175 +29,173 @@ use Symfony\Component\Validator\Constraints;
 /**
  * Class CategoryType
  */
-class CategoryType extends AbstractType
-{
-    use EntityTranslatableFormTrait, FactoryTrait;
+class CategoryType extends AbstractType {
+	use EntityTranslatableFormTrait, FactoryTrait;
 
-    protected $imageNamespace;
+	protected $imageNamespace;
 
-    public function __construct(
-        $imageNamespace
-    ) {
-        $this->imageNamespace = $imageNamespace;
-    }
+	public function __construct(
+		$imageNamespace
+	) {
+		$this->imageNamespace = $imageNamespace;
+	}
 
-    /**
-     * Configures the options for this type.
-     *
-     * @param OptionsResolver $resolver The resolver for the options.
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'empty_data' => function () {
-                $this
-                    ->factory
-                    ->create();
-            },
-            'data_class' => $this
-                ->factory
-                ->getEntityNamespace(),
-        ]);
-    }
+	/**
+	 * Configures the options for this type.
+	 *
+	 * @param OptionsResolver $resolver The resolver for the options.
+	 */
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			'empty_data' => function () {
+				$this
+					->factory
+					->create();
+			},
+			'data_class' => $this
+				->factory
+				->getEntityNamespace(),
+		]);
+	}
 
-    /**
-     * Buildform function
-     *
-     * @param FormBuilderInterface $builder the formBuilder
-     * @param array                $options the options for this form
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $currentCategoryId = $builder
-            ->getData()
-            ->getId();
+	/**
+	 * Buildform function
+	 *
+	 * @param FormBuilderInterface $builder the formBuilder
+	 * @param array                $options the options for this form
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$currentCategoryId = $builder
+			->getData()
+			->getId();
 
-        $categoryNamespace = $this
-            ->factory
-            ->getEntityNamespace();
+		$categoryNamespace = $this
+			->factory
+			->getEntityNamespace();
 
-        $builder
-            ->add('name', 'text', [
-                'required' => true,
-                'constraints' => [
-                    new Constraints\Length(
-                        [
-                            'max' => 65,
-                        ]
-                    ),
-                ],
-            ])
-            ->add('slug', 'text', [
-                'required' => false,
-                'constraints' => [
-                    new Constraints\Length(
-                        [
-                            'max' => 65,
-                        ]
-                    ),
-                ],
-            ])
-            ->add('root', 'checkbox', [
-                'required' => false,
-            ])
-            ->add('cssClass', 'text', [
-                'required' => false,
-            ])
-            ->add('showInHome', 'checkbox', [
-                'required' => false,
-            ])
-            ->add('enabled', 'checkbox', [
-                'required' => false,
-            ])
-            ->add('metaTitle', 'text', [
-                'required' => false,
-            ])
-            ->add('metaDescription', 'text', [
-                'required' => false,
-                'constraints' => [
-                    new Constraints\Length(
-                        [
-                            'max' => 159,
-                        ]
-                    ),
-                ],
-            ])
-            ->add('metaKeywords', 'text', [
-                'required' => false,
-            ])
-            ->add('imagesSort', 'text', [
-                'required' => false,
-            ])
-            ->add('images', 'entity', [
-                'class' => $this->imageNamespace,
-                'required' => false,
-                'property' => 'id',
-                'multiple' => true,
-                'expanded' => false,
-            ])
-            ->add('icons', 'entity', [
-                'class' => $this->imageNamespace,
-                'required' => false,
-                'property' => 'id',
-                'multiple' => true,
-                'expanded' => false,
-            ])
-            ->add('attachments', 'entity', [
-                'class' => 'Elcodi\Component\Media\Entity\Attachment',
-                'required' => false,
-                'property' => 'id',
-                'multiple' => true,
-                'expanded' => false,
-            ])
-            ->add('parent', 'entity', [
-                'class' => $categoryNamespace,
-                'query_builder' => $this->getAvailableCategories($currentCategoryId),
-                'required' => true,
-                'multiple' => false,
-            ])
-        ;
+		$builder
+			->add('name', 'text', [
+				'required' => true,
+				'constraints' => [
+					new Constraints\Length(
+						[
+							'max' => 65,
+						]
+					),
+				],
+			])
+			->add('description', CKEditorType::class, [
+				'required' => false,
+				'config_name' => 'my_config',
+			])
+			->add('slug', 'text', [
+				'required' => false,
+				'constraints' => [
+					new Constraints\Length(
+						[
+							'max' => 65,
+						]
+					),
+				],
+			])
+			->add('root', 'checkbox', [
+				'required' => false,
+			])
+			->add('cssClass', 'text', [
+				'required' => false,
+			])
+			->add('showInHome', 'checkbox', [
+				'required' => false,
+			])
+			->add('enabled', 'checkbox', [
+				'required' => false,
+			])
+			->add('metaTitle', 'text', [
+				'required' => false,
+			])
+			->add('metaDescription', 'text', [
+				'required' => false,
+				'constraints' => [
+					new Constraints\Length(
+						[
+							'max' => 159,
+						]
+					),
+				],
+			])
+			->add('metaKeywords', 'text', [
+				'required' => false,
+			])
+			->add('imagesSort', 'text', [
+				'required' => false,
+			])
+			->add('images', 'entity', [
+				'class' => $this->imageNamespace,
+				'required' => false,
+				'property' => 'id',
+				'multiple' => true,
+				'expanded' => false,
+			])
+			->add('icons', 'entity', [
+				'class' => $this->imageNamespace,
+				'required' => false,
+				'property' => 'id',
+				'multiple' => true,
+				'expanded' => false,
+			])
+			->add('attachments', 'entity', [
+				'class' => 'Elcodi\Component\Media\Entity\Attachment',
+				'required' => false,
+				'property' => 'id',
+				'multiple' => true,
+				'expanded' => false,
+			])
+			->add('parent', 'entity', [
+				'class' => $categoryNamespace,
+				'query_builder' => $this->getAvailableCategories($currentCategoryId),
+				'required' => true,
+				'multiple' => false,
+			])
+		;
 
-        $builder->addEventSubscriber($this->getEntityTranslatorFormEventListener());
-    }
+		$builder->addEventSubscriber($this->getEntityTranslatorFormEventListener());
+	}
 
-    /**
-     * This method returns a closure used to show only the valid categories to
-     * be selected as parent.
-     *
-     * @param integer|null $currentCategoryId The current category id
-     *
-     * @return callable
-     */
-    protected function getAvailableCategories($currentCategoryId)
-    {
-        return function (CategoryRepository $categoryRepository) use ($currentCategoryId) {
+	/**
+	 * This method returns a closure used to show only the valid categories to
+	 * be selected as parent.
+	 *
+	 * @param integer|null $currentCategoryId The current category id
+	 *
+	 * @return callable
+	 */
+	protected function getAvailableCategories($currentCategoryId) {
+		return function (CategoryRepository $categoryRepository) use ($currentCategoryId) {
 
-            return $categoryRepository->getAvailableParentCategoriesQueryBuilder($currentCategoryId);
-        };
-    }
+			return $categoryRepository->getAvailableParentCategoriesQueryBuilder($currentCategoryId);
+		};
+	}
 
-    /**
-     * Returns the prefix of the template block name for this type.
-     *
-     * The block prefix defaults to the underscored short class name with
-     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
-     *
-     * @return string The prefix of the template block name
-     */
-    public function getBlockPrefix()
-    {
-        return 'elcodi_admin_product_form_type_category';
-    }
+	/**
+	 * Returns the prefix of the template block name for this type.
+	 *
+	 * The block prefix defaults to the underscored short class name with
+	 * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+	 *
+	 * @return string The prefix of the template block name
+	 */
+	public function getBlockPrefix() {
+		return 'elcodi_admin_product_form_type_category';
+	}
 
-    /**
-     * Return unique name for this form
-     *
-     * @deprecated Deprecated since Symfony 2.8, to be removed from Symfony 3.
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->getBlockPrefix();
-    }
+	/**
+	 * Return unique name for this form
+	 *
+	 * @deprecated Deprecated since Symfony 2.8, to be removed from Symfony 3.
+	 *
+	 * @return string
+	 */
+	public function getName() {
+		return $this->getBlockPrefix();
+	}
 }
